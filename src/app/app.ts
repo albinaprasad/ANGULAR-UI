26523e6eb934f8f8ad1a4baeeea6907da606523a
Albin Ap <albin.ap@gmail.com>
2025-12-26 19:28:50 +0530

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,17 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    LoginComponent
+],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('my-angular-app');
+}
---
+++ src/app/login/login.ts
@@ -0,0 +1,45 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const body = document.querySelector("body") as HTMLElement;
+    const modal = document.querySelector(".modal") as HTMLElement;
+    const modalButton = document.querySelector(".modal-button") as HTMLElement;
+    const closeButton = document.querySelector(".close-button") as HTMLElement;
+    const scrollDown = document.querySelector(".scroll-down") as HTMLElement;
+    let isOpened = false;
+
+    const openModal = () => {
+      modal.classList.add("is-open");
+      body.style.overflow = "hidden";
+    };
+
+    const closeModal = () => {
+      modal.classList.remove("is-open");
+      body.style.overflow = "initial";
+    };
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > window.innerHeight / 3 && !isOpened) {
+        isOpened = true;
+        scrollDown.style.display = "none";
+        openModal();
+      }
+    });
+
+    modalButton.addEventListener("click", openModal);
+    closeButton.addEventListener("click", closeModal);
+
+    document.onkeydown = evt => {
+      evt = evt || window.event;
+      evt.keyCode === 27 ? closeModal() : false;
+    };
+  }
+}
